218c8445420594aae5135b47862173b7e4ad6851
Marcio Casagrande <mmlcasag@gmail.com>
2020-07-06 16:43:07 -0300

    C33A496 - Type Inference & Type Casting

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-// we used the ".ts" extenstion because it is a typescript file
-// first of all, we need to install the typescript compiler
-// npm install -g typescript
-// then you can run tsc app.ts
-// and then this will convert the typescript file into a js file
-// so you can notice a new app.js file in the project folder
-// and then, o the index.html, you can import the .js file
-// if you're having trouble trying to run tsc app.ts
-// watch this video https://www.youtube.com/watch?v=J30n3lMyvbY
-// now we want to specify the data type for these arguments
-// and they should be number
-// so how do we do that?
+var num1Element = document.getElementById('num1'); // type casting
+var num2Element = document.getElementById('num2'); // type casting
+var buttonElement = document.querySelector('button');
 function add(num1, num2) {
     return num1 + num2;
 }
-// which data types are possible?
-// number, string, boolean, object, Array
-console.log(add(3, 2));
-console.log(add('3', '2'));
+buttonElement.addEventListener('click', function () {
+    var num1 = num1Element.value; // know we know that we have a value property in the HTMLInputElement
+    var num2 = num2Element.value; // know we know that we have a value property in the HTMLInputElement
+    var result = add(+num1, +num2); // raises error if i don't explicitly convert it to number
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -1,32 +1,14 @@
-// we used the ".ts" extenstion because it is a typescript file
-// first of all, we need to install the typescript compiler
-// npm install -g typescript
-// then you can run tsc app.ts
-// and then this will convert the typescript file into a js file
-// so you can notice a new app.js file in the project folder
-// and then, o the index.html, you can import the .js file
-// if you're having trouble trying to run tsc app.ts
-// watch this video https://www.youtube.com/watch?v=J30n3lMyvbY
+const num1Element = document.getElementById('num1') as HTMLInputElement; // type casting
+const num2Element = document.getElementById('num2') as HTMLInputElement; // type casting
+const buttonElement = document.querySelector('button');
 
-// now we want to specify the data type for these arguments
-// and they should be number
-// so how do we do that?
 function add(num1: number, num2: number) {
     return num1 + num2;
 }
 
-// which data types are possible?
-// number, string, boolean, object, Array
-
-console.log(add(3, 2));
-
-// now, when compiling, you will see an error in the line above
-console.log(add('3', '2'));
-// it will compile the js file though
-// but it will show an error message during compilation
-// you can prevent typescript from generating the js file in the configurations
-
-// but its basically it
-// it's more to prevent these types of errors during development 
-// and correct them before it becomes a thing
-// this is the main feature of typescript
+buttonElement.addEventListener('click', () => {
+    const num1 = num1Element.value; // know we know that we have a value property in the HTMLInputElement
+    const num2 = num2Element.value; // know we know that we have a value property in the HTMLInputElement
+    const result = add(+num1, +num2); // raises error if i don't explicitly convert it to number
+    console.log(result);
+});
