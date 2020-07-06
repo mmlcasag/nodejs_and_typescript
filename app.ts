2b96cb0256160547745b4ce30ee2ac73fb374de4
Marcio Casagrande <mmlcasag@gmail.com>
2020-07-06 19:48:38 -0300

    C33A504 - Getting Started With Node and TypeScript

---
+++ app.ts
@@ -0,0 +1,10 @@
+// to make this work when working with typescript
+// you have to install this: npm install --save-dev @types/node
+// this lets typescript know nodejs specific commands and how to translate them into javascript
+import express from 'express';
+
+// this lets typescript know express, its functions, classes, objects and it helps intellisense
+// npm install --save-dev @types/express
+const app = express();
+
+app.listen(3000);
